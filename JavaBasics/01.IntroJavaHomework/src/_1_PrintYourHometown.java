22259e53798056f29cd9439784c887876a0387c3
goodlifeinc <genata93@gmail.com>
2014-09-25 10:24:28 +0300

    The solutions in java format.

---
+++ JavaBasics/01.IntroJavaHomework/src/_1_PrintYourHometown.java
@@ -0,0 +1,6 @@
+
+public class _1_PrintYourHometown {
+	public static void main(String[] args) {
+		System.out.println("My hometown is Haskovo.");
+	}
+}
---
+++ JavaBasics/01.IntroJavaHomework/src/_5_PrintTheCurrentDate.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+
+public class _5_PrintTheCurrentDate {
+	public static void main(String[] args) {
+		LocalDateTime date = LocalDateTime.now();
+		System.out.println(date);
+	}
+}
